22ff4becec9a6bbbe83efaeb1eb1765d803df5ac
Julia <118815057+Iulia-kld@users.noreply.github.com>
2024-03-06 14:26:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from faker import Faker
 from telebot import TeleBot, types
 
 # TODO: вставить свой токен
-TOKEN = 'AAGUbtoibohw8BEBfDIZTvVqueG3VTFmdDc'
+TOKEN = 'здесь должен быть твой токен'
 bot = TeleBot(TOKEN, parse_mode='html')
 # библиотека для генерации тестовых ФИО
 # указываем язык - русский
